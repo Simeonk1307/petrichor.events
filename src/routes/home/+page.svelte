a1e09cb2233d34bcf641565539b38b15ec41cb4e
Devansh Kesan <devanshkesan101@gmail.com>
2024-07-01 21:27:38 +0530

    events call

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,3 +1,12 @@
-<div>
+<script>
+    // import Member from "../../lib/components/Workshop/WorkshopCompnt.svelte";
+    import Member2 from "../../lib/components/event/Event.svelte";
+
+</script>
+
+<Member2/>
+
+<!-- <div>
     <h1>HOME PAGE</h1>
-</div>
+</div> -->
+
