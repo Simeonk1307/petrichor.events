948f1439299441b9d4f8cfdc5a1b64c0c45fe2a5
Petrichor-events <events.petrichor@iitpkd.ac.in>
2024-09-10 20:53:05 +0530

    Update index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,7 @@
 
-const backend_url = 'http://127.0.0.1:8000/'
+// const backend_url = 'http://127.0.0.1:8000/'
 // const backend_url = 'https://petri-back.vercel.app/'
-// const backend_url = 'https://petrichor-backend.vercel.app/'
+const backend_url = 'https://petrichor-backend.vercel.app/'
 
 export const API = {
     login: backend_url + 'api/login/',
