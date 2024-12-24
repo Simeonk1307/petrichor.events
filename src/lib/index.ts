d1c9ce7dd0bf42d45fc6deb4d24c13c469c3379c
alonot <priyanshup27092004@gmail.com>
2024-12-24 12:30:08 +0530

    change in config

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 
-const backend_url = 'http://127.0.0.1:8000/'
-// const backend_url = 'https://petri-back.vercel.app/'
+// const backend_url = 'http://127.0.0.1:8000/'
+const backend_url = 'https://petri-back.vercel.app/'
 // const backend_url = 'https://petrichor-backend.vercel.app/'
 
 export const frontend_url = 'http://127.0.0.1:5173/'
