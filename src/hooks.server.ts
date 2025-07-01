730cbed8d160427a7a8229808da8afb65c4330b9
Gaureesh JR <137698759+gebriish@users.noreply.github.com>
2025-07-01 23:14:20 +0530

    frontend route filtering (#7)
    * feat: frontend route filtering
    
    * fix: adjust route filter list
    
    * feat: added error route for hooks redirecting

---
+++ src/hooks.server.ts
@@ -0,0 +1,32 @@
+import { error, type Handle } from '@sveltejs/kit';
+
+// INFO(gebriish): allowed routes go here
+const frontendRoutes = [
+	'/',
+	'/home',
+	'/aboutus',
+	'/login',
+	'/register',
+	'/changepassword',
+	'/forgotpassword',
+	'/ca/welcome',
+	'/ca/profile',
+	'/error'
+];
+
+
+export const handle: Handle = ({ event, resolve }) => {
+
+	const path = event.url.pathname.toLowerCase();
+
+	const isAllowed = frontendRoutes.some(route =>
+		path === route || path.startsWith(route + '/')
+	);
+
+  if (!isAllowed) {
+		const errorUrl = new URL("/error", event.url)
+		return Response.redirect(errorUrl, 302);
+  }
+
+	return resolve(event);
+};
