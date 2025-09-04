1d2bd8768703623f26036aa5a2d1f45545252db9
Abhishek Iyer <abhi.iyer10@gmail.com>
2025-09-04 18:59:50 +0530

    Added new images

--- src/lib/data/workshop.ts
+++ src/lib/data/workshop.ts
@@ -1,12 +1,10 @@
-import img1 from '$lib/assets/Workshop/pcb.jpg'
-import img2 from '$lib/assets/Workshop/ui.jpg'
-import img3 from '$lib/assets/Workshop/appdev.jpg'
-import img4 from '$lib/assets/Workshop/fusion_360.png'
-import img5 from '$lib/assets/Workshop/ml.png'
-import img6 from '$lib/assets/Workshop/Iot.png'
-import web from '$lib/assets/Workshop/web.png'
-import ethical from '$lib/assets/Workshop/ethical.png'
-import digital from '$lib/assets/Workshop/digital.png'
+import pcb from '$lib/assets/Workshop/pcb.webp'
+import ui from '$lib/assets/Workshop/ui.webp'
+import appdev from '$lib/assets/Workshop/appdev.webp'
+import matlab from '$lib/assets/Workshop/matlab.webp'
+import mlops from '$lib/assets/Workshop/mlops.webp'
+import mlintro from '$lib/assets/Workshop/mlintro.webp'
+import web from '$lib/assets/Workshop/web.webp'
 import type { workshop } from '$lib/types'
 
 let closed_workshop = {
@@ -18,7 +16,7 @@ let closed_workshop = {
         "description":"Calling all Innovators! Have you ever wanted to transform your ideas into real, working apps? This workshop is your gateway to app development! Learn to build powerful mobile applications using React Native and JavaScript in a hands-on, beginner-friendly environment. No matter if you're just starting out or aiming to sharpen your coding skills, this workshop will guide you step by step. By the end of the session, you'll design your very own interactive app, explore server integration, and gain confidence in bringing your projects to life!",
         "prerequisites":["A Laptop (optional: with Android Studio)","Basic Programming Skills"],
         "topics":["App Dev","React-Native"],
-        "image":img3,
+        "image":appdev,
         "price":1199
     },
     "WP02": {
@@ -40,7 +38,7 @@ let closed_workshop = {
         "description":"Hello, Creative Thinkers! Have you ever wondered what makes a website or app user-friendly and visually appealing? User Interface (UI) and User Experience (UX) design are the keys to crafting digital experiences that not only look great but also work seamlessly. In this workshop, you'll dive into the principles of good design, learn how to create intuitive interfaces, and explore the importance of user feedback. But that's not all! You'll also get hands-on experience with designing your own UI/UX project, putting theory into practice.",
         "prerequisites":["A laptop with a mouse","Curiosity and a passion for design!"],
         "topics":["Machine Leaning"],
-        "image":img2,
+        "image":mlintro,
         "price":1199
     },
     "WP04": {
@@ -51,7 +49,7 @@ let closed_workshop = {
         "description":"Hello, Aspiring Engineers and Designers! Ready to revolutionize your approach to design and manufacturing? This hands-on, immersive workshop will transform your skills in Fusion CAD and Generative Design. Explore key concepts like sketching, modeling, drafting, and assembly. You’ll master advanced tools and techniques in Fusion CAD and unlock the potential of generative design to optimize your creations like never before. By the end of this workshop, you’ll have practical experience in developing intricate designs and assemblies, along with a comprehensive understanding of how generative design can drive innovation in your projects. Get ready to elevate your design skills to the next level!",
         "prerequisites":["Basic familiarity with Fusion CAD or any design software https://www.autodesk.com/learn/ondemand/course/learn-fusion-for-cad-in-90-minutes " ],
         "topics":["MATLAB"],
-        "image":img4,
+        "image":matlab,
         "price":999
     },
     "WP05": {
@@ -62,7 +60,7 @@ let closed_workshop = {
         "description":"Hello, Creative Thinkers! Have you ever wondered what makes a website or app user-friendly and visually appealing? User Interface (UI) and User Experience (UX) design are the keys to crafting digital experiences that not only look great but also work seamlessly. In this workshop, you'll dive into the principles of good design, learn how to create intuitive interfaces, and explore the importance of user feedback. But that's not all! You'll also get hands-on experience with designing your own UI/UX project, putting theory into practice.",
         "prerequisites":["A laptop with a mouse","Curiosity and a passion for design!"],
         "topics":["UI/UX"],
-        "image":img2,
+        "image":ui,
         "price":1199
     },
     "WP06": {
@@ -73,7 +71,7 @@ let closed_workshop = {
         "description":"Hi Makers! Join our 2-day PCB Design Workshop and dive into the world of electronics! Learn the basics of PCB design, key terminologies, and the workflow from schematic to layout. Get hands-on with KiCAD, explore the fabrication process, and practice soldering SMD and THT components. By the end, you’ll be ready to design and assemble your own PCB!  ",
         "prerequisites":["A laptop with KiCad software installed."],
         "topics":["PCB"],
-        "image":img1,
+        "image":pcb,
         "price":1099
     },
     "WP07": {
@@ -84,7 +82,7 @@ let closed_workshop = {
         "description":"Ever wondered how machine learning models go from development to real-world deployment? This workshop will guide you through every step of the MLOps pipeline! From data engineering and preparing training data to applying feature engineering, model development, and deployment, you'll get hands-on experience at each stage. We’ll also explore how to keep models up-to-date with continual learning. By the end of this workshop, you'll have the skills to build, deploy, and maintain ML models like a pro!",
         "prerequisites":["'Intro to Machine Learning' Workshop attended" , "A Laptop with Python and Jupyter Notebook installed"],
         "topics":["Ml & Neural Networks"],
-        "image":img5,
+        "image":mlops,
         "price":1199
     },
 }
@@ -99,7 +97,7 @@ export let workshops:{[id: string]:workshop} = {
         "description":"Calling all Innovators! Have you ever wanted to transform your ideas into real, working apps? This workshop is your gateway to app development! Learn to build powerful mobile applications using React Native and JavaScript in a hands-on, beginner-friendly environment. No matter if you're just starting out or aiming to sharpen your coding skills, this workshop will guide you step by step. By the end of the session, you'll design your very own interactive app, explore server integration, and gain confidence in bringing your projects to life!",
         "prerequisites":["A Laptop (optional: with Android Studio)","Basic Programming Skills"],
         "topics":["App Dev","React-Native"],
-        "image":img3,
+        "image":appdev,
         "price":1199
     },
     "WP02": {
@@ -121,7 +119,7 @@ export let workshops:{[id: string]:workshop} = {
         "description":"Hi Makers! Join our 2-day PCB Design Workshop and dive into the world of electronics! Learn the basics of PCB design, key terminologies, and the workflow from schematic to layout. Get hands-on with KiCAD, explore the fabrication process, and practice soldering SMD and THT components. By the end, you’ll be ready to design and assemble your own PCB!  ",
         "prerequisites":["A laptop with KiCad software installed."],
         "topics":["PCB"],
-        "image":img1,
+        "image":pcb,
         "price":1099
     },
     "WP07": {
@@ -132,7 +130,7 @@ export let workshops:{[id: string]:workshop} = {
         "description":"Ever wondered how machine learning models go from development to real-world deployment? This workshop will guide you through every step of the MLOps pipeline! From data engineering and preparing training data to applying feature engineering, model development, and deployment, you'll get hands-on experience at each stage. We’ll also explore how to keep models up-to-date with continual learning. By the end of this workshop, you'll have the skills to build, deploy, and maintain ML models like a pro!",
         "prerequisites":["'Intro to Machine Learning' Workshop attended" , "A Laptop with Python and Jupyter Notebook installed"],
         "topics":["Ml & Neural Networks"],
-        "image":img5,
+        "image":mlops,
         "price":1199
     }
 }
